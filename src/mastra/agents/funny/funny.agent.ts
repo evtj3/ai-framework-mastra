f106f0794275a07e515ad34385acbb026a7968cc
Eriberto Tabada Jr <eribertotabada@dnamicro.com>
2025-05-23 14:34:29 +0800

    add read me and revise the directory structure

--- src/mastra/agents/funny/funny.agent.ts
+++ src/mastra/agents/funny/funny.agent.ts
@@ -2,12 +2,12 @@ import { google } from "@ai-sdk/google";
 import { Agent } from "@mastra/core/agent";
 import { Memory } from "@mastra/memory";
 import { LibSQLStore } from "@mastra/libsql";
-import { funny_instruction3 } from "../../instructions";
+import { funny_instruction1 } from "../../instructions";
 // import { getRandomFunnyResponse, detectTriggers } from '../tools';
 
 export const funnyAgent = new Agent({
   name: "FunnyAgent",
-  instructions: funny_instruction3,
+  instructions: funny_instruction1,
   model: google("gemini-1.5-pro-latest"),
   // memory: new Memory({
   //     storage: new LibSQLStore({
